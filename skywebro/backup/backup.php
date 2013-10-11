07af9a99e45eca0549f3e961cec1db27b1527a1c
andi <andi@dev.skyweb.ro>
2013-10-11 05:18:51 +0300

    moved to namespace folder

---
+++ skywebro/backup/backup.php
@@ -0,0 +1,25 @@
+<?php
+define('DIRECTORY_MASK', 0777);
+
+spl_autoload_extensions('.class.php');
+spl_autoload_register();
+
+use Skywebro\Backup\Backup;
+
+$exit_code = 0;
+$ini = getopt('i:')['i'];
+
+if (empty($ini)) {
+    print "Usage: backup -i ini_file\n";
+    $exit_code = 1;
+} else {
+    try {
+        $backup = Backup::factory($ini);
+        $backup->run();
+    } catch (Exception $e) {
+        print 'ERROR: ' . $e->getMessage() . "!\n";
+        $exit_code = 2;
+    }
+}
+
+exit($exit_code);
