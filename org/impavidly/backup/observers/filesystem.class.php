846d9a260456817e9d1727111a69e22232f38dd4
Andi Trînculescu <andi@skyweb.ro>
2014-03-16 16:08:53 +0200

    solved name clashes

--- org/impavidly/backup/observers/filesystem.class.php
+++ org/impavidly/backup/observers/filesystem.class.php
@@ -12,7 +12,8 @@ class Filesystem extends Common {
         while ($directory->valid()) {
             if (!$directory->isDot() && ($directory->isDir())) {
                 try {
-                    $command = "{$subject->config['filesystem']['tar']} -jcf {$subject->outputPath}/" . $directory->getFilename() . ".tar.bz2 --directory=" . $directory->getPathname() . " .";
+                    $archiveName = $subject->outputPath . DIRECTORY_SEPARATOR . str_replace(DIRECTORY_SEPARATOR, '_', trim($directory->getPathname(), DIRECTORY_SEPARATOR)) . '_' . $directory->getFilename() . '.tar.bz2';
+                    $command = "{$subject->config['filesystem']['tar']} -jcf $archiveName --directory=" . $directory->getPathname() . " .";
                     $subject->logger->info("{$this->name}: running {$command}");
                     $this->execute($subject, $command);
                 } catch (\Exception $e) {
