e0b25f5dd2b6299304610efac92ed3507bfbc6b7
Andi Trînculescu <andi@skyweb.ro>
2014-03-16 18:41:39 +0200

    error handling

--- org/impavidly/backup/observers/filesystem.class.php
+++ org/impavidly/backup/observers/filesystem.class.php
@@ -5,22 +5,31 @@ class Filesystem extends Common {
     protected $name = 'Filesystem Observer';
 
     public function update(\SplSubject $subject) {
-        $path = $subject->data[0];
-        $directory = new \FilesystemIterator($path);
+        $path = $subject->data[0];        
         $status = 0;
         
-        while ($directory->valid()) {
-            if (!$directory->isDot() && ($directory->isDir())) {
-                try {
-                    $archiveName = $subject->outputPath . DIRECTORY_SEPARATOR . str_replace(DIRECTORY_SEPARATOR, '_', trim($directory->getPathname(), DIRECTORY_SEPARATOR)) . '_' . $directory->getFilename() . '.tar.bz2';
-                    $command = "{$subject->config['filesystem']['tar']} -jcf $archiveName --directory=" . $directory->getPathname() . " .";
-                    $subject->logger->info("{$this->name}: running {$command}");
-                    $this->execute($subject, $command);
-                } catch (\Exception $e) {
-                    $status = -1;
+        try {
+            $directory = new \DirectoryIterator($path);
+            while ($directory->valid()) {
+                if (!$directory->isDot() && ($directory->isDir())) {
+                    try {
+                        $archiveName = $subject->outputPath . DIRECTORY_SEPARATOR . str_replace(DIRECTORY_SEPARATOR, '_', trim($directory->getPathname(), DIRECTORY_SEPARATOR)) . '_' . $directory->getFilename() . '.tar.bz2';
+                        $command = "{$subject->config['filesystem']['tar']} -jcf $archiveName --directory=" . $directory->getPathname() . " .";
+                        $subject->logger->info("{$this->name}: running {$command}");
+                        $this->execute($subject, $command);
+                    } catch (\Exception $e) {
+                        $status = -1;
+                    }
                 }
+                $directory->next();
             }
-            $directory->next();
+        } catch (\UnexpectedValueException $e) {
+            $subject->logger->warn("{$this->name}: could not open ($path).");
+            $status = -1;
+            
+        } catch (\Exception $e) {
+            $subject->logger->warn("{$this->name}: unknown error.");
+            $status = -1;
         }
 
         return $status;
